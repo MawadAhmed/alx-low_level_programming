6dabda3b4a20b029c9d9d4704b4824316398e9bb
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-10 18:28:20 -0700

    puts in C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /*
  * first C program
  */
-int main()
+int main(void)
 {
-	char mycod[]="Programming is like building a multilingual puzzle";
+	char mycod[] = "Programming is like building a multilingual puzzle";
 		puts(mycod);
-		return 0;
+		return (0);
 }
