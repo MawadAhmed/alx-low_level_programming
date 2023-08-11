88bba1aee9462be8e49536d79d690aea75ee835d
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-10 22:59:23 -0700

    error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -8,6 +9,7 @@
 int main(void)
 {
 	char qou[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, qou, 59);
 	return (1);
 }
