dd5e312b713f3233ecb1bfdb059f5db3b8cb612f
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-10 21:00:00 -0700

    error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+}
