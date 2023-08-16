f64b82316380f49377eaf3ab1986fcec45d4805c
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-16 08:21:52 -0700

    commit 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet.
+ *
+ */
+print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		char str;
+
+		for (str = 'a' ; str <= 'z' ; str++)
+		{
+			_putchar(str);
+		_putchar('\n');
+		}
+	}
+}
