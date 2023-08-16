ce8ca7a3688052af2ff27f1e07ff6f175ec11adc
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-16 05:11:33 -0700

    commit 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,14 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char str[] = "_putchar";
 	int i;
 
 	for (i = 0 ; i < 8 ; i++)
-	{
 		_putchar(str[i]);
-		_putchar('\n');
-	}
+	_putchar('\n');
 	return (0);
 }
