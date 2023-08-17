582a3f46eaccc4d2c8a2dd62261b77c368bfc869
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-17 08:20:54 -0700

    commit 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - function that checks for uppercase character.
+ *
+ * @c : check the input of the function.
+ *
+ * Return: return 1 if c is upercase otherwise return 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}
+
