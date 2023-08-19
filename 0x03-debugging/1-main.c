d6fd75217ffa1fc07e3bec04d1039fe90eeeca48
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-19 06:59:26 -0700

    commit 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,15 +7,14 @@
 
 int main(void)
 {
-	int i;
-
+	int i
+		;
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 
 	while (i < 10)
 	{
-		putchar(i);
-		/**
+		putchar(i);/**
 		 * Infinite loop incoming :(
 		 * Infinite loop avoided! \o/
 		 */
