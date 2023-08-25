f5a50e819b82f75fd9703ebf9aa6bedab368674a
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-25 16:23:24 -0700

    commit 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
- * _strcat - function concatenates two string
+ * _strncat - function concatenates two string
  *
  * @dest: pointer to destion input
  * @src: pointer to source input
- *
+ * @n: integer input
  * Return: returns a pointer to the resulting string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int s1, s2;
+	int s1, s2;
 
-        s1 = 0;
+	s1 = 0;
 
-        while (dest[s1])
-                s1++;
+	while (dest[s1])
+		s1++;
 
-        for (s2 = 0 ; src[s2] = n ; src++)
-                dest[s1++] = src[s2];
-        return (dest);
-}
+	for (s2 = 0 ; s2 < n && src[s2] != '\0' ; src++)
+		dest[s1 + s2] = src[s2];
+	dest[s1 + s2] = '\0';
+	return (dest);
+}}
