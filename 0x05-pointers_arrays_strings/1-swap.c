a02cbf453ea700772b9ca81bd5a705e453301f2c
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-25 09:47:10 -0700

    commit 0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int -  function swaps the values of two integers.
+ *
+ * @a : int parameter.
+ * @b : int parameter.
+ *
+ * Returun: nothing.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
