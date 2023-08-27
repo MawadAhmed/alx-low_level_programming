6154898e69bd54548f21e92392552f50c2b52b49
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-27 13:23:25 -0700

    commit 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _atoi - function that convert a string to an integer.
+ *
+ * @s : string pointer input.
+ *
+ * Return: return integer number with sign.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int res = 0;
+	int i = 0;
+
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++
+	}
+	else if (s[i] >= '0' &&  s[i] <= '9')
+	{
+		for (i = 0; s[i] != '\0' ; i++)
+		{
+			res = res * 10 + s[i] - '0';
+		}
+	}
+	else
+		break;
+	return (res * sign);
+}
