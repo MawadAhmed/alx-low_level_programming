9af73221c57b53b7637e35efb425ee9653c9e6fc
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-27 13:39:03 -0700

    commit 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,18 +11,19 @@ int _atoi(char *s)
 	int sign = 1;
 	int res = 0;
 	int i = 0;
-
-	if (s[i] == '-')
+	
+	for (i = 0; s[i] != '\0' ; i++)
 	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] >= '0' &&  s[i] <= '9')
-		for (i = 0; s[i] != '\0' ; i++)
+		if (s[i] == '-')
+		{
+			sign = -1;
+		}
+		else if (s[i] >= '0' &&  s[i] <= '9')
 		{
 			res = res * 10 + s[i] - '0';
 		}
-	else if
-		break;
+		else
+			break;
+	}
 	return (res * sign);
 }
