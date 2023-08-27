3e3e80d6570aed02b2582fe87f3e0fe44d5c0972
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-27 14:19:26 -0700

    commit 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - program that generates random valid passwords.
+ * 
+ * Return : return 0;
+ */
+int main(void)
+{
+	int sum;
+	char p;
+
+	srand(time(NULL));
+	while(sum <= 2000)
+	{
+		p = rand() % 120;
+		sum =  sum + p;
+		printf("%c",p);
+	}
+	printf(3456 - sum);
+	return (0);
+}
