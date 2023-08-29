609dd9b8f28e3ffd42dd5595817962a60dbdc3f4
Mawada Ahmed <mowadahmd@gmail.com>
2023-08-28 17:04:16 -0700

    commit 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,14 +9,13 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, *p;
+	int i, j;
 
 	for (i = 0 ; haystack[i] > '\0'; i++)
 	{
 		for (j = 0; needle[j] > '\0' && needle[j] == haystack[i]; j++)
 		{
-			p = &haystack[i];
-			return (p);
+			return (haystack);
 		}
 	}
 	return ('\0');
