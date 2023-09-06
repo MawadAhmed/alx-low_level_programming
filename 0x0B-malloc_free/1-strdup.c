a0f730b99d838eb7159d70a2dede6e8fb8f8aa0c
Mawada Ahmed <mowadahmd@gmail.com>
2023-09-05 19:54:04 -0700

    commit 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,14 +9,15 @@
 char *_strdup(char *str)
 {
 	int i, size;
+	char *s;
 
 	if (str == NULL)
 		return (NULL);
 	for (size = 0; str[size] != '\0'; size++)
 		;
-	char *s = malloc(size * sizeof(*str) + 1);
+	s = malloc(size * sizeof(*str) + 1);
 
-	if (m == 0)
+	if (s == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		s[i] = str[i];
